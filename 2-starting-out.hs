ab31a5e24f122541778841ade79cd2b282157171
Andrei Misarca <misarcaa@amazon.com>
2015-07-15 12:27:22 +0100

    Completed ex 2

--- 2-starting-out.hs
+++ 2-starting-out.hs
@@ -14,43 +14,42 @@ penultimate l = last (init l)
 
 -- Find the element at index k in list l
 -- For example: "findK 2 [0,0,1,0,0,0]" returns 1
-findK k l = undefined
+findK k l = l !! k
 
 -- Determine if list l is a palindrome
-isPalindrome l = undefined
+isPalindrome l = l == reverse l
 
 {-
  - Duplicate the elements in list xs, for example "duplicate [1,2,3]" would give the list [1,1,2,2,3,3]
- - Hint: The "concat [l]" function flattens a list of lists into a single list. 
+ - Hint: The "concat [l]" function flattens a list of lists into a single list.
  - (You can see the function definition by typing ":t concat" into the interpreter. Perhaps try this with other variables and functions)
  -
  - For example: concat [[1,2,3],[3,4,5]] returns [1,2,3,3,4,5]
  -}
-duplicate xs = undefined
+duplicate xs = concat [[x, x] | x <- xs]
 
 {-
  - Imitate the functinality of zip
  - The function "min x y" returns the lower of values x and y
  - For example "ziplike [1,2,3] ['a', 'b', 'c', 'd']" returns [(1,'a'), (2, 'b'), (3, 'c')]
  -}
-ziplike xs ys = undefined
+ziplike xs ys = [(findK x xs, findK x ys) | x <- [0..min (length xs) (length ys)-1]]
 
 -- Split a list l at element k into a tuple: The first part up to and including k, the second part after k
 -- For example "splitAtIndex 3 [1,1,1,2,2,2]" returns ([1,1,1],[2,2,2])
-splitAtIndex k l = undefined
-
+splitAtIndex k l = ([findK x l | x <- [0..k-1]], [findK x l | x <- [k..length l-1]])
 -- Drop the element at index k in list l
 -- For example "dropK 3 [0,0,0,1,0,0,0]" returns [0,0,0,0,0,0]
-dropK k l = undefined
+dropK k l = [findK x l | x <- [0..k-1]] ++ [findK x l | x <- [k+1..length l-1]]
 
 -- Extract elements between ith and kth element in list l. Including i, but not k
 -- For example, "slice 3 6 [0,0,0,1,2,3,0,0,0]" returns [1,2,3]
-slice i k l = undefined
+slice i k l = [findK x l | x <- [i..k-1]]
 
 -- Insert element x in list l at index k
 -- For example, "insertElem 2 5 [0,0,0,0,0,0]" returns [0,0,0,0,0,2,0]
-insertElem x k l = undefined
+insertElem x k l = [findK x l | x <- [0..k-1]] ++ [x] ++ [findK x l | x <- [k..length l-1]]
 
 -- Rotate list l n places left.
 -- For example, "rotate 2 [1,2,3,4,5]" gives [3,4,5,1,2]
-rotate n l = undefined
+rotate n l = [findK x l | x <- [n..length l-1]] ++ [findK x l | x <- [0..n-1]]
